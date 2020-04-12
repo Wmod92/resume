4ab959b4abb6ddce0b28fb433097d781877314c8
Wmod92 <kovalev.oleg@list.ru>
2020-04-12 14:15:55 +0300

    щл

--- ask-work.js
+++ ask-work.js
@@ -1,3 +1,17 @@
-function valid() {
+function valid(form) {
+var fail = false;
+var name = form.name.value;
+var telephon = form.telephon.value;
+if (name == "" || name == " ") {
+  fail = "Вы не ввели свое имя";
+}
+if (telephon == "" {
+  fail = "Вы не ввели номер телефона";
+}
+if (fail) {
+  alert (fail);
+  return false;
+}
 
+}
 }
